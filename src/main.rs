d3a3e444efe78785d0fc9506fb1b73ce54b699c3
Oliver Fleckenstein <oliverfl@live.dk>
2023-02-06 14:42:17 +0000

    feat: Sending email

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,78 @@
 use thirtyfour::prelude::*;
 use tokio::time::{sleep, Duration};
 
+use lettre::{
+    transport::smtp::{
+        authentication::{Credentials, Mechanism},
+        extension::ClientId,
+    },
+    Message, SmtpTransport, Transport,
+};
+
 const URL: &str = "https://whatson.bfi.org.uk/imax/Online/default.asp";
 
 #[tokio::main]
 async fn main() -> WebDriverResult<()> {
+    let email_options = EmailOptions::create_from_environment();
+    match email_options {
+        Ok(opts) => {
+            send_email(&opts);
+        }
+        Err(e) => {
+            panic!("Email options missing: {:?}", e);
+        }
+    }
     Ok(())
 }
 
+#[derive(Debug)]
+struct EmailOptions {
+    email: String,
+    password: String,
+    server: String,
+}
+
+impl EmailOptions {
+    pub fn create_from_environment() -> Result<Self, std::env::VarError> {
+        use std::env;
+        let email = env::var("EMAIL")?;
+        let password = env::var("EMAIL_PASSWORD")?;
+        let server = env::var("EMAIL_SERVER")?;
+
+        Ok(Self {
+            email,
+            password,
+            server,
+        })
+    }
+}
+
+fn send_email(options: &EmailOptions) {
+    let message = Message::builder()
+        .from(format!("Me <{}>", options.email).parse().unwrap())
+        .reply_to(format!("Yuin <{}>", options.email).parse().unwrap())
+        .to(format!("Hei <{}>", options.email).parse().unwrap())
+        .subject("Happy new year")
+        .body(String::from("Be happy!"))
+        .unwrap();
+
+    let creds = Credentials::new(options.email.to_string(), options.password.to_string());
+
+    let mailer = SmtpTransport::starttls_relay(options.server.as_str())
+        .unwrap()
+        .hello_name(ClientId::Domain("localhost".to_string()))
+        .credentials(creds)
+        .authentication(vec![Mechanism::Login])
+        .build();
+
+    // Send the email
+    match mailer.send(&message) {
+        Ok(_) => println!("Email sent successfully!"),
+        Err(e) => println!("Could not send email: {:?}", e),
+    }
+}
+
+/// Visit BFI's website to check if tickets are on sale yet.
 async fn check_if_tickets_are_on_sale() -> WebDriverResult<bool> {
     let mut caps = DesiredCapabilities::chrome();
     caps.set_headless()?;
