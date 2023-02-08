402191583de8d16284132611ec0adf68e12f642d
Oliver Fleckenstein <oliverfl@live.dk>
2023-02-08 21:42:09 +0000

    feat: Logging with timestamp + different subjects on email

--- script.py
+++ script.py
@@ -2,6 +2,7 @@
 import os
 import asyncio
 import smtplib
+import logging
 from email.message import EmailMessage
 from playwright.async_api import async_playwright
 from asyncio import sleep
@@ -10,6 +11,7 @@ from typing import List
 URL = "https://whatson.bfi.org.uk/imax/Online/default.asp"
 DELAY_SEC = 1800  # 30 minutes = 60 seconds * 30
 
+logging.basicConfig(filename="ant.log", format='%(asctime)s %(message)s', level=logging.INFO)
 
 async def main():
     email = os.environ.get("OUTLOOK_EMAIL")
@@ -18,28 +20,31 @@ async def main():
 
     if not email or not email_password or not receivers:
         print("Either email or password is missing")
-        return
+        exit(1)
 
+    logging.info("Starting server")
     while True:
         try:
             if await check_if_tickets_are_on_sale():
-                print("Tickets are on sale")
+                logging.info("Tickets are on sale")
                 send_email(
                     email,
                     email_password,
                     [email, receivers],
-                    f"YES! Tickets are on sale now, go buy them immediately!\n\nYou can by them here: {URL}",
+                    "YES! Tickets are on sale, go buy them now!",
+                    f"YES! Tickets are on sale now, go buy them immediately!\n\nYou can by them at {URL}",
                 )
             else:
-                print("Not on sale yet")
+                logging.info("Not on sale yet")
                 send_email(
                     email,
                     email_password,
                     [receivers],
+                    f"Are Ant-man tickets on sale yet?",
                     f"No, they are still not on sale...\n\nYou can check here: {URL}",
                 )
         except Exception as e:
-            print(str(e))
+            logging.error(str(e))
 
         await sleep(DELAY_SEC)
 
@@ -55,7 +60,7 @@ async def check_if_tickets_are_on_sale() -> bool:
         await sleep(1)
 
         while not await month.is_visible():
-            print("Feburary not visible")
+            logging.debug("Feburary not visible")
             button = frame.get_by_title("Next month")
             await button.click()
             await sleep(1)
@@ -70,12 +75,12 @@ async def check_if_tickets_are_on_sale() -> bool:
         return isAvailable
 
 
-def send_email(from_email: str, email_password: str, to_email: List[str], content: str):
+def send_email(from_email: str, email_password: str, to_email: List[str], subject: str, content: str):
     msg = EmailMessage()
     msg.set_content(content)
     msg["From"] = from_email
     msg["To"] = to_email
-    msg["Subject"] = f"Are Ant-man tickets on sale yet?"
+    msg["Subject"] = subject
 
     server = smtplib.SMTP("smtp-mail.outlook.com", port=587)
     server.ehlo()
